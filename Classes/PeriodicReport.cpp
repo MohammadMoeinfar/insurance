6449f3d30be09658a893b5dbf8fa7953babadaee
MohammadMoeinfar <m.moeinfar68@gmail.com>
2018-05-15 17:57:50 +0430

    add class Insurance Report

--- Classes/PeriodicReport.cpp
+++ Classes/PeriodicReport.cpp
@@ -23,21 +23,21 @@ Scene *PeriodicReport::createScene() {
 }
 
 PeriodicReport *PeriodicReport::initPeriodicReport() {
-    PeriodicReport* dailyOperation = new PeriodicReport();
+    PeriodicReport* periodicReport = new PeriodicReport();
 
-    if(dailyOperation && dailyOperation->init())
+    if(periodicReport && periodicReport->init())
     {
-        dailyOperation->visibleSize = Director::getInstance()->getVisibleSize();
-        dailyOperation->origin = Director::getInstance()->getVisibleOrigin();
+        periodicReport->visibleSize = Director::getInstance()->getVisibleSize();
+        periodicReport->origin = Director::getInstance()->getVisibleOrigin();
 
         auto layerColor = LayerColor::create(Color4B(245, 247, 249, 255));
-        dailyOperation->addChild(layerColor);
+        periodicReport->addChild(layerColor);
 
         auto mainTitle = LabelTTF::create(TITLE_PERIODIC_REPORT, MAINFONT, 30);
-        mainTitle->setPosition(Vec2(dailyOperation->visibleSize.width - 20, dailyOperation->visibleSize.height - mainTitle->getContentSize().height - 50));
+        mainTitle->setPosition(Vec2(periodicReport->visibleSize.width - 20, periodicReport->visibleSize.height - mainTitle->getContentSize().height - 50));
         mainTitle->setAnchorPoint(Vec2::ANCHOR_MIDDLE_RIGHT);
         mainTitle->setColor(Color3B::BLACK);
-        dailyOperation->addChild(mainTitle);
+        periodicReport->addChild(mainTitle);
 
         string textArray [] = {MILEAGE,
                                MILEAGE_UNIT,
@@ -62,20 +62,20 @@ PeriodicReport *PeriodicReport::initPeriodicReport() {
             titles->setPosition(Vec2(mainTitle->getPositionX(), mainTitle->getPositionY() - 150 - (titles->getContentSize().height * i)));
             titles->setAnchorPoint(Vec2::ANCHOR_MIDDLE_RIGHT);
             titles->setColor(Color3B::BLACK);
-            dailyOperation->addChild(titles);
+            periodicReport->addChild(titles);
 
             auto backField = ImageView::create("backField.png");
             backField->setContentSize(Size(200, 35));
             backField->setScale9Enabled(true);
             backField->setPosition(Vec2(backField->getContentSize().width - 80, mainTitle->getPositionY() - 150 - (titles->getContentSize().height * i)));
-            dailyOperation->addChild(backField);
+            periodicReport->addChild(backField);
 
             auto values = LabelTTF::create();
             values->setPosition(Vec2(backField->getPositionX(), backField->getPositionY()));
             values->setColor(Color3B::BLACK);
             values->setFontName(MAINFONT);
             values->setFontSize(30);
-            dailyOperation->addChild(values);
+            periodicReport->addChild(values);
 
             switch (i)
             {
@@ -119,9 +119,9 @@ PeriodicReport *PeriodicReport::initPeriodicReport() {
             }
         }
 
-        return dailyOperation;
+        return periodicReport;
     }
 
-    CC_SAFE_DELETE(dailyOperation);
+    CC_SAFE_DELETE(periodicReport);
     return nullptr;
 }
