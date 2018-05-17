678b7510570196a4695f91900048f5f59843b02f
MohammadMoeinfar <m.moeinfar68@gmail.com>
2018-05-17 12:15:50 +0430

    add Class ConnectivityReceiver for check internet Connection

--- proj.android-studio/app/src/org/cocos2dx/cpp/ConnectivityReceiverListener.java
+++
@@ -1,9 +0,0 @@
-package org.cocos2dx.cpp;
-
-/**
- * Created by Solyman on 5/17/2018.
- */
-
-public interface ConnectivityReceiverListener {
-    void onNetworkConnectionChanged(boolean isConnected);
-}
